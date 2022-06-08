6c602cb451a11ba7498f511d4b357f7584dd1d99
cheryl <cheryls@brdata.com>
2022-06-08 18:13:16 -0400

    installed EntityFramework. added Models folder

---
+++ Patients/Models/Patient.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Patients.Models
+{
+    public class Patient
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Birthday { get; set; }
+        public char Gender { get; set; }
+    }
+}
